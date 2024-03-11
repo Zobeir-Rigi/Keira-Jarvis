de14655b5ccf30a01d2d9b9fa6d0ef4f606eff3f
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-11 12:27:38 +0000

    revising var and let

--- week2/day1.js
+++ week2/day1.js
@@ -4,4 +4,29 @@ sum = (a,b)=> {
 
 let a = 10;
 let b = 3;
-console.log(sum(a,b));
+// console.log(sum(a,c));
+
+
+const myName = "Saeid"
+// myName = "Saeed";
+// console.log(myName)
+
+function exampleVar() {
+    if (true) {
+      var x = 10;
+      console.log(x); 
+    }
+    console.log(x); 
+  }
+
+// example();
+
+function exampleLet() {
+    if (true) {
+      let y = 20;
+      console.log(y); 
+    }
+    // console.log(y); 
+  }
+  
+   exampleLet();
