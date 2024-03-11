435c15a3fb72a6cbf77b2fce4d664732d802dba3
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-11 15:39:04 +0000

    moved files

---
+++ week2/day1/operators.js
@@ -0,0 +1,24 @@
+function sum (a,b){
+   return a += b
+}
+
+let a = 4
+let b = 3
+console.log(`The sum of a + b is equal to ${sum(a,b)}`)
+
+console.log ("a - b = ", a-b)
+console.log ("a * b = ", a*b)
+console.log("% is the remainder of division so a % b = ", a%b)
+
+const myName = "Saeid";
+console.log("Hello" + `\t` + myName); 
+
+// const a = 4, b = 3, myName = "Saeid";
+// sum = (a, b) => a + b;
+
+// console.log(`Sum = ${sum(a, b)}\n`, `a - b= ${a - b}\n`, `a * b= ${a * b}\n`, `Remainder= ${a % b}\n`);
+// console.log(`Hello\t${myName}`);
+
+
+
+
