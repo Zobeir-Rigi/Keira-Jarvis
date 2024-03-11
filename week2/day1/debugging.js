d1edd5e367db62a48ab8f4c695947ebdacfaef12
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-11 15:35:13 +0000

    debugging

---
+++ week2/day1/debugging.js
@@ -0,0 +1,63 @@
+ 
+1. 
+
+let name = "Alice"; 
+
+console.log ("Hello,"  + name) // name should br the variable so it should be out of double qout and also console is not correct.
+2. 
+
+const age = 25; // there is a space 
+
+console.log ('Age:', age); 
+
+3.  
+
+var isStudent = true; // there is a space in variable name
+
+console.log("Student status: ", isStudent); // need a ,
+
+4 
+let result = 5 + 10; // 10 is a string here
+
+console.log('Result:', result); 
+5. 
+
+var temperature = 30 
+
+if (temperature > 25) { //condition  should be in ( )
+
+  console.log("It's warm outside."); 
+
+} 
+
+6.  
+let greeting = 'Good morning'; 
+let time = 11
+if (time < 12) { // time is not defined
+
+  console.log(greeting) 
+
+} 
+
+7.  
+const pi = 3.14; 
+// pi = 3.14159;  its a const
+
+console.log('PI:', pi); 
+
+8. 
+ let count = 5; 
+
+while (count >= 0) { //condition should be in ()
+  console.log('Count:', count); 
+  count--; 
+} 
+
+9. 
+for (let i = 0; i < 3; i++) { // need simicolon;
+  console.log('Number:', i); 
+} 
+
+10. 
+let colors = ['red', 'green', 'blue' ]
+console.log('First color:', colors[0]); 
