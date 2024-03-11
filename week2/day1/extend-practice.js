b3858a7c863b10c8aacb6875445cc18807b738c0
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-11 16:29:38 +0000

    reviising javaScript

---
+++ week2/day1/extend-practice.js
@@ -0,0 +1,32 @@
+1.
+console.log("I am learning JavaScript!" )
+
+2.
+let a = 7 , b = 3
+let sum = a += 2
+console.log(`a + b = ${sum}`)
+
+3.
+let currentTime = 13
+let c = true;
+if (currentTime === c){
+    console.log("Good Afternoon!")
+} else {
+    console.log("Good Morning")
+}
+
+4.
+console.log("the typeof c is ",typeof c);
+
+5.
+console.log("the number is "+ a);
+
+6.
+let d = 10
+console.log("d is = "+d , "d+10 is = ",d+10) 
+
+7.
+const f = [favoriteColor = "blue", favoriteMovie = "Mad Max", release = 2020]
+console.log(f[0],f[1], f[2]);
+
+8.
