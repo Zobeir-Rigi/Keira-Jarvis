cb00e7b1be2c315a67b0b123a3a9913c0fa43993
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-12 12:59:35 +0000

    Operators

---
+++ week2/day2/exercise3.js
@@ -0,0 +1,8 @@
+let age = 16
+let specialEvent = true
+
+if ( age === 17 && specialEvent ){
+    console.log( "Welcome, and Enjoy your time." )
+} else if ( age < 18 || !specialEvent){
+    console.log ( " You are under 18, but u don't have a ticket for the special event." )
+}
