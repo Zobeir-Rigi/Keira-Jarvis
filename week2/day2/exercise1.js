544ace5feb0e494c75d86d9f05169fe50720811a
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-12 13:03:37 +0000

    folder renamed

--- week2/day2/exercise1.js
+++
@@ -1,6 +0,0 @@
-let age = 17
-
-if( age === 18 || age > 18 ){
-    console.log("Welcome to adulthood")
-}else
-console.log("Enjoy your youth!")
--- week2/day2/exercise3.js
+++
@@ -1,8 +0,0 @@
-let age = 16
-let specialEvent = true
-
-if ( age === 17 && specialEvent ){
-    console.log( "Welcome, and Enjoy your time." )
-} else if ( age < 18 || !specialEvent){
-    console.log ( " You are under 18, but u don't have a ticket for the special event." )
-}
