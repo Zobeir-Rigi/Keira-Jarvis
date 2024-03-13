8ace269af9bcaad5e4551c32f7235a32d0bc23e8
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-13 10:42:26 +0000

    reverse

--- week3/day3/forLoop.js
+++ week3/day3/forLoop.js
@@ -2,12 +2,12 @@
 
 let greetings = ['Hello', 'Hi', 'Hey', 'Howdy', 'Greetings'];
 console.log(greetings.length)
-for ( let i = 0 ; i < greetings.length ; i++){
+for ( let i = greetings.length-1 ; i >= 0 ; i--){
     console.log(`${greetings[i]}, Saeid`);
 }
 
-greetings.forEach(function (greet){
-    console.log(greet)
-} )
+// greetings.forEach(function (greet){
+//     console.log(greet)
+// } )
 
-greetings.forEach(greet => console.log(greet))
+// greetings.forEach(greet => console.log(greet))
