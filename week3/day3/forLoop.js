7db73c9b5229816acf5512aab58e06ed6d213642
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-13 10:28:16 +0000

    loop revising

--- week3/day3/forLoop.js
+++ week3/day3/forLoop.js
@@ -1,3 +1,7 @@
-for ( let i = 0 ; i < 5 ; i++) {
-    console.log("Hello Javascript!");
+
+
+let greetings = ['Hello', 'Hi', 'Hey', 'Howdy', 'Greetings'];
+console.log(greetings.length)
+for ( let i = 0 ; i < greetings.length ; i++){
+    console.log(`${greetings[i]}, Saeid`);
 }
