38251e711a15e4c2c013383bb585b66b33c3c011
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-14 11:21:57 +0000

    revised

--- week3/day4/array.js
+++ week3/day4/array.js
@@ -1,2 +1,10 @@
 let arr = ['Apple', 'Banana',true, 1 ]
 console.log(arr[1])
+arr.push(12.1, 'orange')
+console.log("add to the end of arr", arr)
+arr.unshift('start')
+console.log("Added to the begining of arr", arr)
+arr.pop()
+console.log("removed last item",arr)
+arr.splice(1,0,'KIWI')
+console.log("Added kiwi to the index 1 of our arr", arr)
