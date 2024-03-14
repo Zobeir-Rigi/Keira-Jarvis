2cb5e525b9ea48a8c77193ac6ea2a01a4b786705
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-14 12:36:14 +0000

    array Method

---
+++ week3/day4/arrayMethod.js
@@ -0,0 +1,33 @@
+// .shift() .unshift() .slice() .splice() .concat() .join() .map() .filter() .reduce() .forEach() 
+
+let frontend = ['HTML', 'CSS', 'Javascript', 'react.js', 'Next.js', 'Typescript', 'angular']
+let shiftedArr = frontend.shift()
+console.log(`${shiftedArr} removed from frontend list => ${frontend}`)
+
+let unShiftedArr = frontend.unshift('HTML5')
+console.log(`new length is ${unShiftedArr} frontend list => ${frontend}`)
+
+let slicedArr = frontend.slice(3,5)
+console.log(`slicedArr is =>${slicedArr}`);
+
+let spliceedArr = frontend.splice(2,0,'Tailwind CSS')
+console.log(`slicedArr is ${frontend}`)
+
+let backend = ['Sql', 'Node.js']
+let concatedArr = frontend.concat(backend)
+console.log(`concatedArr => ${concatedArr}`);
+
+let joinedArr = frontend.join('*')
+console.log(`joinedArr => ${joinedArr}`)
+
+let mapedArr = frontend.map(item => console.log(item))
+
+let filteredArr = concatedArr.filter(backend => backend == "Node.js")
+console.log(`filteredArr =>${filteredArr}`)
+
+let numbers = [2,5,6]
+let reducedArr = numbers.reduce((acc, cur) => acc * cur)
+console.log(`reducedArr => ${reducedArr}`)
+
+frontend.forEach(item => console.log(item))
+
