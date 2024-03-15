11c310606afa4bed1b46cbef8a0effcfc3c9c822
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-15 09:52:08 +0000

    reduce

--- week3/day4/subSum.js
+++ week3/day4/subSum.js
@@ -1,5 +1,8 @@
-let a = [10, 2]
-console.log( a.reduce((a, b) => a + b))
-console.log( a.reduce((a, b) => a * b))
-console.log( a.reduce((a, b) => a - b))
-console.log( a.reduce((a, b) => a / b))
+// let a = [10, 2]
+// console.log( a.reduce((a, b) => a + b))
+// console.log( a.reduce((a, b) => a * b))
+// console.log( a.reduce((a, b) => a - b))
+// console.log( a.reduce((a, b) => a / b))
+
+let a = [10, 2];
+console.log(a.reduce((a, b) => a + b), a.reduce((a, b) => a * b), a.reduce((a, b) => a - b), a.reduce((a, b) => a / b));
