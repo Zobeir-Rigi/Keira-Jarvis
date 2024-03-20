dc0db6e6d1b1dc8db21219fb7a227eff6680394f
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-20 15:57:24 +0000

    converted to a anonymous func

--- week3/day4/function.js
+++ week3/day4/function.js
@@ -1,6 +1,9 @@
-function combineWords (strOne, strtwo){
-      return strOne.concat(" ",strtwo)
-}
+// function combineWords (strOne, strtwo){
+//       return strOne.concat(" ",strtwo)
+// }
 let a = "Hello"
 let b = "dear!"
+// console.log(combineWords(a, b))
+
+const combineWords = () => strOne.concat(" ",strtwo)
 console.log(combineWords(a, b))
