643b135cf774fb26221f2afae9de3e4ba2b049cf
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-20 16:00:17 +0000

    parameter added

--- week3/day4/function.js
+++ week3/day4/function.js
@@ -5,5 +5,5 @@ let a = "Hello"
 let b = "dear!"
 // console.log(combineWords(a, b))
 
-const combineWords = () => strOne.concat(" ",strtwo)
+const combineWords = (strOne, strTwo) => strOne.concat(" ", strTwo)
 console.log(combineWords(a, b))
