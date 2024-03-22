040ef7bbe7a6f03ad4d6117b4fef3c6e6088265c
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-22 16:46:30 +0000

    Math.max

--- week3/day4/minmax.js
+++ week3/day4/minmax.js
@@ -2,3 +2,8 @@ function findMin (numbers) {
     return Math.min(...numbers)
 }
 console.log(findMin([10,20,40]))
+
+function findMax (numbers) {
+    return Math.max(...numbers)
+}
+console.log(findMin([10,20,40]))
