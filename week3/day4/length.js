360dca148354c6960e41070277241ee999c779f1
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-23 21:27:51 +0000

    another way

--- week3/day4/length.js
+++ week3/day4/length.js
@@ -1,9 +1,9 @@
 function islongerThan (string, length) {
-    //  if (string.length> length) {
-    //     return true
-    // } else {
-    //     return false
-    // }
-    return string.length> length
+     if (string.length> length) {
+        return true
+    } else {
+        return false
+    }
+    // return string.length> length
 }
 console.log(islongerThan(4,2))
