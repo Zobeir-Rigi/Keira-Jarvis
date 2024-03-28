e78f3f0f793462af857b520f1ef6a8b9bdd7a417
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-28 14:38:20 +0000

    i could console.log index 0

--- Week5/day4/fetch.js
+++ Week5/day4/fetch.js
@@ -1,19 +1,26 @@
+// const abortController = new AbortController()
 
+
+// const signal = abortController.signal
 fetch ('http://jsonplaceholder.typicode.com/users')
 .then(response=>response.json())
 .then(users=>{
     const userCard = document.getElementById('userCard')
-
     users.forEach(user=>{
         console.log(user.name)
        const userDatails = document.createElement('div')
        userDatails.classList.add('user-details')
         userDatails.innerHTML = `
-        <div > name: ${user.name}</div>
-        <div > email: ${user.email}</div>
+        <div class='name' > name : ${user.name}</div>
+        <div > email : ${user.email}</div>
+        <div > Adderss : ${user.address.city}</div>
+        <div > Website : ${user.website}</div>
         `
         userCard.appendChild(userDatails)
 
     })
 })
 .catch(error => console.error('error fetching users:', error))
+// setTimeout(()=>{
+//     abortController.abort()
+// },5000)
