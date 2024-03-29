908040e92e6f2dbf4368471b7d6888557bfcf328
zobeir-rigi <rigi.zobir@yahoo.com>
2024-03-29 16:40:55 +0000

    userName Added

--- Week5/day4/fetch.js
+++ Week5/day4/fetch.js
@@ -13,9 +13,11 @@ fetch ('https://jsonplaceholder.typicode.com/users')
         userDatails.innerHTML = `
         <img  src = "https://lh3.googleusercontent.com/proxy/ydAXQDqT2RPBYd-7xKLOYqFCBODxKfw4bH4NXkex5bm2mcESn2PSuUQvurMasw5WRgw2EIyqkSazMfZYNjD7eQGYFdw3n2xJ0E7G6CclObeYcKi5aDeLMw">
         <div class='name' > name : ${user.name}</div>
+        <div class='name' > name : ${user.username}</div>
         <div > email : ${user.email}</div>
         <div > Adderss : ${user.address.city}</div>
         <div > Website : ${user.website}</div>
+
         `
         userCard.appendChild(userDatails)
 
