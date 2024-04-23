4dae97d6c8318cdeba0da4820b8d7a05680406e0
zobeir-rigi <rigi.zobir@yahoo.com>
2024-04-23 16:17:16 +0100

    function

---
+++ week9/day2/function.py
@@ -0,0 +1,10 @@
+def userName():  
+    fullName = input("Enter your name: ")
+    address = input("Enter your address: ")
+    interest = input("Any interest? ")
+    print(f"my name is {fullName}, my address is {address} and my interest is {interest}")
+
+# fullName, address, interest = userName()
+# print(f"my name is {fullName}, my address is {address} and my interest is {interest}")
+
+userName()
---
+++ week9/day2/subtract.py
@@ -0,0 +1,17 @@
+def subtract ():
+    firtNumber = int(input("Enter the first number: "))
+    secondNumber = int(input("Enter the second number: "))
+
+    result = firtNumber - secondNumber
+    print (f"{firtNumber - secondNumber } = {result}")
+
+def multiply ():
+    firtNumber = int(input("Enter the first number: "))
+    secondNumber = int(input("Enter the second number: "))
+
+    result = firtNumber * secondNumber
+    print (f"{firtNumber * secondNumber } = {result}")
+
+
+subtract()
+multiply()
