d01ec12d818693726e3504961adfed2f5b887ac8
zobeir-rigi <rigi.zobir@yahoo.com>
2024-04-24 10:51:41 +0100

    capitilizw, title and swapcase

---
+++ week9/day3/stringHandlingTask.py
@@ -0,0 +1,46 @@
+"Modify/Make"
+
+# Note: files for the tasks below are in the 
+
+
+# To Do:
+
+# Task 1 Working with comparison operators" # != , == , <= , >=, <,>
+#  use any comparison operator to compare the letter "a" and "A"
+#  use any comparison operator to compare the letters "ax" and "ZZ"
+#  use any comparison operator to compare your firstname with any another first name
+
+
+# Task 2
+# Create two variables fName and lName and join and print them using a variable called fullName
+fName = "saeid"
+lName :"rigi"
+fullName = fName + lName
+
+# Task 3: print a specific character using the index value or position in a string or list?
+course = "Python"
+findChar = course[1]
+print(findChar)
+
+
+# Task 4: Write the code to print the letter h from the variable below 
+course = "Python"
+char = "h"
+for c in course:
+    if c == char:
+        print(c)
+
+# Extension activity: Research and independent learning activity
+
+# Task 1: Return all the characters from the string held in the course variable using negative values
+
+# Task 2: Research and independent learning activity
+# Open the Python documentation using the provided link to discover uses of :
+str.capitalize()
+str.title()
+str.swapcase()
+https://docs.python.org/3.3/library/stdtypes.html?highlight=substring#string-methods
+text = "Hello World"
+swapped_text = text.swapcase()
+print(swapped_text)  # Output: "hELLO wORLD"
+
