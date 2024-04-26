d603b8ebbcd34f009889de787db7677286c6f2b8
zobeir-rigi <rigi.zobir@yahoo.com>
2024-04-26 11:15:49 +0100

    read done

--- week9/Music_DB_App/readrecords.py
+++ week9/Music_DB_App/readrecords.py
@@ -8,4 +8,16 @@
 "" '' # Notes
 "" '' # The SQL statement may be parametrized (i. e. placeholders instead of SQL literals). 
 "" '' # A parameter specifies the value a particular field must contain when carrying out a query. 	
-
+from connect import *  # import everything from the connect.py file
+ 
+def read():
+    try:
+        cursor.execute("SELECT * FROM songs") # selects all the records from the songs table
+        row = cursor.fetchall() # obtain all the records and they are stored in the row variable
+        for aRecord in row:
+            print(aRecord)    
+    except sql.OperationalError as e:
+        print(f"Records not found: {e}")
+ 
+if __name__ == "__main__":
+    read()
