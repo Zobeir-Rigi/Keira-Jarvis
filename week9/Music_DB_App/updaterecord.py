6b049ac9453f1d97fd7c5752331e10718b4c2c34
zobeir-rigi <rigi.zobir@yahoo.com>
2024-04-26 12:25:39 +0100

    update completed

--- week9/Music_DB_App/updaterecord.py
+++ week9/Music_DB_App/updaterecord.py
@@ -4,3 +4,28 @@
 "" '' # Use try and except to handle error(s)
 "" '' # Use the execute method from the cursor object to run sql statement
 
+from connect import *
+ 
+def update():
+        idField = input("Enter the ID of the record to be updated: ") # int()
+ 
+        fieldName = input("Enter the field to be updated (Title, Artist, Genre): ").title() # capitalise() in JS
+        print(fieldName)
+        newValue = input(f"Enter the value for {fieldName}: ") # python string output
+        #print the value to check  
+ 
+        # newValue = "'"+newValue+"'"
+ 
+        try:
+            cursor.execute(f"UPDATE songs SET {fieldName} = '{newValue}' WHERE SongID = {idField}")
+            conn.commit()
+            print(f"Record {idField} has been updated")
+               
+ 
+        except sql.OperationalError as e:  
+            conn.rollback()
+            print(f"Record not updated: {e}")
+ 
+ 
+if __name__ == "__main__":
+     update()
